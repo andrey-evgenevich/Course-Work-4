e4066b07a183a0a3160025ac9c44ab392eb4d383
andrey sokolov <andrey.evgenevich.s@gmail.com>
2024-07-21 15:25:23 +0800

    Описана основная функция

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
-# Создание экземпляра класса для работы с API сайтов с вакансиями
-hh_api = HeadHunterAPI()
-
-# Получение вакансий с hh.ru в формате JSON
-hh_vacancies = hh_api.get_vacancies("Python")
-
-# Преобразование набора данных из JSON в список объектов
-vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
-
-# Пример работы контструктора класса с одной вакансией
-vacancy = Vacancy("Python Developer", "", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
-
-# Сохранение информации о вакансиях в файл
-json_saver = JSONSaver()
-json_saver.add_vacancy(vacancy)
-json_saver.delete_vacancy(vacancy)
-
-# Функция для взаимодействия с пользователем
-def user_interaction():
-    platforms = ["HeadHunter"]
-    search_query = input("Введите поисковый запрос: ")
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
-
-    filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
-
-    ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
-
-    sorted_vacancies = sort_vacancies(ranged_vacancies)
-    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    print_vacancies(top_vacancies)
+from config import VACANCIES_PATH_TXT, VACANCIES_PATH_JSON
+from classes.json_saver import JSONSaver
+from classes.txt_saver import TXTSaver
+from utils import user_choice_json, user_choice_txt
 
 
-if __name__ == "__main__":
-    user_interaction()
+def main():
+    user_input = input("Здравствуйте!\n"
+                       "В каком формате записать данные?\n"
+                       "json формат, введите 1\n"
+                       "txt формат, введите 2\n"
+                       "Для удаления данных из файла, введите 3\n")
+
+    if user_input == "1":
+        user_choice_json()
+    elif user_input == "2":
+        user_choice_txt()
+    elif user_input == "3":
+        user_input = input("Какой файл вы отчистить?\n"
+                           "json-файл, введите 1\n"
+                           "txt-файл, введите 2\n")
+        if user_input == "1":
+            deleter = JSONSaver(VACANCIES_PATH_JSON)
+            deleter.del_data()
+            print("Данные удалены!")
+        elif user_input == "2":
+            deleter = TXTSaver(VACANCIES_PATH_TXT)
+            deleter.del_data()
+            print("Данные удалены!")
+    return
+
+
+if __name__ == '__main__':
+    main()
